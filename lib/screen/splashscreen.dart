60e1e656bcfd770cf1f24ca17568fcb37376c2c9
dhias <dhiasa01@gmail.com>
2024-11-08 15:10:06 +0700

    Baru Halaman Awal

---
+++ lib/screen/splashscreen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:wisata_mobile_5/screen/landingpage.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //menunda sebanyak 3 detik untuk menampilkan Splashscreen sebelum berpindah halaman
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LandingPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffFFFFFF),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // Lottie Animation, pastikan URL valid atau pakai animasi lokal
+            Lottie.network(
+              'https://lottie.host/956e1e4f-8c98-4206-ae82-50dd50161d69/dtw01aXDDE.json',
+              width: 350,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // SizedBox to create vertical spacing
+                SizedBox(height: 300), // This creates a vertical space of 300 pixels
+                Text(
+                  "RIZAL RINALDI",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff1B1B1B),
+                  ),
+                ),
+                Text(
+                  "SLIPKNOT",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff1B1B1B),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
